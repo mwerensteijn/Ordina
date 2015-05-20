9764f035b8bc05ab99892c13e79633258255676b
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-05-20 18:11:33 +0200

    PictureHunt init Containing: - A simple crosshair - Shooting "bullets" - Basic level (almost empty) - Cardboard prefab

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpawner : MonoBehaviour {
+    public Rigidbody bullet;
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Shoot()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * speed;
+    }
+}
---
+++ Assets/Scripts/ControlController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlController : MonoBehaviour {
+    public BulletSpawner bulletSpawner;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            bulletSpawner.Shoot();
+        }
+	}
+}
