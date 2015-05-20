0c3db6e2ff245e2748788499369350f666c40b54
mwerensteijn <mitchell.werensteijn@student.hu.nl>
2015-05-20 23:02:23 +0200

    Moved MultipleChoice game scripts
    Moved scripts to match the directory structure

--- Assets/Scripts/AirplaneMovement.cs
+++ Assets/Scripts/AirplaneMovement.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
+	private enum Lane { Left, Middle, Right };
+	private Lane currentLane = Lane.Middle;
+
+	private Lane newLane = Lane.Middle;
+	private Lane lookLane = Lane.Middle;
+
 	private float movementSpeed = 10;
 	private float sideMovementSpeed = 15;
 	public GameObject head;
@@ -18,10 +24,29 @@ public class Movement : MonoBehaviour {
 		float y = transform.position.y;
 		float z = transform.position.z + movementSpeed * Time.deltaTime;
 
+		if(head.transform.rotation.y < -0.3f) { 	  // Look left
+			lookLane = 0;
+
+
+			if(currentLane == Lane.Middle) {
+				newLane = Lane.Left;
+			} else if(currentLane == Lane.Right) {
+				newLane = Lane.Middle;
+			}
+		} else if(head.transform.rotation.y > 0.3f) { // Look Right
+			if(currentLane == Lane.Middle) {
+				newLane = Lane.Right;
+			} else if(currentLane == Lane.Left) {
+				newLane = Lane.Middle;
+			}
+		}
+
+
+
 		x += sideMovementSpeed * Time.deltaTime * head.transform.rotation.y * 3.3f;
 
 		transform.position = new Vector3(x, y, z);
 
-		//Debug.Log (head.transform.rotation.y);
+		Debug.Log (currentLane);
 	}
 }
