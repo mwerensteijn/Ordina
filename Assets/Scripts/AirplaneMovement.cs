0ebbfb2c8049abe84517cb2ec69a440287b8fb62
mwerensteijn <mitchell.werensteijn@student.hu.nl>
2015-05-26 12:17:52 +0200

    Answers move

--- Assets/Scripts/AirplaneMovement.cs
+++ Assets/Scripts/AirplaneMovement.cs
@@ -3,18 +3,22 @@ using System.Collections;
 
 public class AirplaneMovement : MonoBehaviour {
 	public enum Position { Left, Middle, Right };
-	public AirplaneMovement.Position answer = Position.Middle;
+	public AirplaneMovement.Position lookingPosition = Position.Middle;
+	
+	private float movementSpeed = 40f;
+	private float sideMovementSpeed = 30f;
+	
+	private float leftLanePositionX = -50f;
+	private float middleLanePositionX = -16f;
+	private float rightLanePositionX = 18f;
 
-	private float movementSpeed = 10;
-	private float sideMovementSpeed = 30;
-
-	private float leftLanePositionX = -48;
-	private float middleLanePositionX = -16;
-	private float rightLanePositionX = 15;
+	public GameObject answers;
+	public float newAnswersPositionZ;
+	public float questionDistance = 250f;
 
 	// Use this for initialization
 	void Start () {
-
+		newAnswersPositionZ = transform.position.z + questionDistance;
 	}
 	
 	// Update is called once per frame
@@ -22,33 +26,58 @@ public class AirplaneMovement : MonoBehaviour {
 		float x = transform.position.x;
 		float y = transform.position.y;
 		float z = transform.position.z + movementSpeed * Time.deltaTime;
-
-		if(answer == Position.Left) {
+		
+		if(lookingPosition == Position.Left) {
 			if(x > leftLanePositionX) {
 				x -= sideMovementSpeed * Time.deltaTime;
+				
+				if(x < leftLanePositionX) {
+					x = leftLanePositionX;
+				}
 			}
-		} else if(answer == Position.Middle) {
-			if(x < middleLanePositionX - (sideMovementSpeed * Time.deltaTime)) {
+		} else if(lookingPosition == Position.Middle) {
+			if(x < middleLanePositionX) {
 				x += sideMovementSpeed * Time.deltaTime;
-			} else if(x > middleLanePositionX + (sideMovementSpeed * Time.deltaTime)) {
+				
+				if(x > middleLanePositionX) {
+					x = middleLanePositionX;
+				}
+			} else if(x > middleLanePositionX) {
 				x -= sideMovementSpeed * Time.deltaTime;
+				
+				if(x < middleLanePositionX) {
+					x = middleLanePositionX;
+				}
 			}
 		} else {
 			if(x < rightLanePositionX) {
 				x += sideMovementSpeed * Time.deltaTime;
+				
+				if(x > rightLanePositionX) {
+					x = rightLanePositionX;
+				}
 			}
 		}
-
+		
 		transform.position = new Vector3(x, y, z);
-	}
 
+		if(transform.position.z >= newAnswersPositionZ) {
+			newAnswersPositionZ += questionDistance;
+			answers.transform.position = answers.transform.position + new Vector3(0, 0, questionDistance);
+		}
+		
+		answers.transform.FindChild("A");
+		answers.transform.FindChild("B");
+		answers.transform.FindChild("C");
+	}
+	
 	void OnTriggerEnter(Collider other) {
 		if(other.name == "A") {
-			answer = AirplaneMovement.Position.Left;
+			lookingPosition = AirplaneMovement.Position.Left;
 		} else if(other.name == "B") {
-			answer = AirplaneMovement.Position.Middle;
+			lookingPosition = AirplaneMovement.Position.Middle;
 		} else if(other.name == "C") {
-			answer = AirplaneMovement.Position.Right;
+			lookingPosition = AirplaneMovement.Position.Right;
 		}
 	}
 }
