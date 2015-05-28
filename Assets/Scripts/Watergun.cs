9963a78f754435fb4871f8a745cd29f4c08e11cc
Jannes <chanan.vanooijen@student.hu.nl>
2015-05-28 15:55:27 +0200

    Added watergun

---
+++ Assets/Scripts/Watergun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watergun : MonoBehaviour {
+
+    public GameObject waterGun;
+
+	// Use this for initialization
+	void Start () {
+        waterGun.GetComponent<EllipsoidParticleEmitter>().emit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Fire2"))
+        {
+            waterGun.GetComponent<EllipsoidParticleEmitter>().emit = false;
+        } 
+	}
+
+    public void Shoot()
+    {
+        waterGun.GetComponent<EllipsoidParticleEmitter>().emit = true;
+
+    }
+}
