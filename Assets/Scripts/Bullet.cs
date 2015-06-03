49e5c6af632f1c4e20682cb43bc26d41d837b376
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-06-03 14:31:25 +0200

    Bullet quads (almost working) instead of planes but is not rotate correctly. Also this seems to cause weird issues like not spawning new bullets after spamming them. Also this cause performance issues

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -12,6 +12,9 @@ public class Bullet : MonoBehaviour {
 
     private float offsetValue = 0.001f;
 
+    private Ray ray;
+    private RaycastHit hit;
+
 	// Use this for initialization
 	void Start () {
 
@@ -24,20 +27,24 @@ public class Bullet : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
-      /*  foreach (ContactPoint contact in collision.contacts)
-        {
-            Instantiate(impact, new Vector3(contact.point.x+0.001f, contact.point.y, contact.point.z), Quaternion.FromToRotation(Vector3.up, contact.normal));
-        }*/
         ContactPoint contact = collision.contacts[0];
         //if (collision.collider.tag.Equals("Answer"))
-       // {
+        // {
+        // Check position of hit with a raycast
+        //ray = Camera.main.ScreenPointToRay(contact.point);
+        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.Log("rayCasted.");
+        }
+
         if (ImpactList.Count > 50)
         {
             GameObject firstImpactInList = ImpactList[0];
             ImpactList.Remove(firstImpactInList);
             Destroy(firstImpactInList);
         }
-        Debug.Log(contact.normal);
+        //Debug.Log(contact.normal);
         //Debug.Log(Quaternion.FromToRotation(Vector3.up, contact.normal));
         if (contact.normal.x > 0)
         {
@@ -75,20 +82,21 @@ public class Bullet : MonoBehaviour {
         {
             zOffset = 0;
         }
+        //Debug.Log(contact.point);
+        //Debug.Log(collision.contacts.Length);
+        //Debug.Log(hit.point);
+        //ImpactList.Add(Instantiate(impact, new Vector3(hit.point.x + xOffset, hit.point.y + yOffset, hit.point.z + zOffset), Quaternion.FromToRotation(Vector3.up, contact.normal)) as GameObject);
         
-        ImpactList.Add(Instantiate(impact, new Vector3(contact.point.x + xOffset, contact.point.y + yOffset, contact.point.z + zOffset), Quaternion.FromToRotation(Vector3.up, contact.normal)) as GameObject);
+       
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Debug.Log(rotation.eulerAngles);
+        // Quad needs to be rotated 90 degrees
+        Quaternion test = Quaternion.Euler(rotation.eulerAngles.x , rotation.eulerAngles.y, rotation.eulerAngles.z);
+        test.eulerAngles = new Vector3(rotation.eulerAngles.x + 270, rotation.eulerAngles.y + 180, rotation.eulerAngles.z);
+        Debug.Log(test.eulerAngles);
+        ImpactList.Add(Instantiate(impact, new Vector3(contact.point.x + xOffset, contact.point.y + yOffset, contact.point.z + zOffset), test) as GameObject);
         Debug.Log(ImpactList.Count);
 
         Destroy(this.gameObject);
     }
-    //void ontriggerenter(collider other)
-    //{
-    //    if (other.tag.equals("answer"))
-    //    {
-    //        instantiate(impact, new vector3(other.transform.position.x + 0.001f, other.transform.position.y, other.transform.position.z), quaternion.fromtorotation(vector3.left, other.transform.eulerangles));
-    //        debug.log("answer");
-    //    }
-    //    //impactdecal bulletclone = (impactdecal) instantiate(impact, other.transform.position, other.transform.rotation);
-    //    destroy(this.gameobject);
-    //}
 }
