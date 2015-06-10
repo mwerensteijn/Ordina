67bfb2ab8e5505423ae688aa2624377ddeb01776
mwerensteijn <mitchell.werensteijn@student.hu.nl>
2015-06-10 15:32:03 +0200

    File upload GUI

--- Assets/GUISkin/Skin.cs
+++ Assets/GUISkin/Skin.cs
@@ -18,20 +18,22 @@ public class Skin : MonoBehaviour {
         GUI.skin = skin;
         
 
-        GUI.Box(new Rect(0, 0, 500, 300), "Open file");
+        GUI.Box(new Rect(0, 0, 500, 340), "Open file");
         GUI.Button(new Rect(477, 3, 21, 21), "X");
         GUI.Label(new Rect(20, 50, 100, 20), "Look in:");
         GUI.TextField(new Rect(120, 50, 200, 20), "C:\\");
-
-        scrollPosition = GUI.BeginScrollView(new Rect(100, 80, 300, 100), scrollPosition, new Rect(0, 0, 300, 300));
         
-        GUI.Button(new Rect(0, 0, 100, 20), "Top-left");
-        GUI.Button(new Rect(120, 0, 100, 20), "Top-right");
-        GUI.Button(new Rect(0, 180, 100, 20), "Bottom-left");
-        GUI.Button(new Rect(120, 180, 100, 20), "Bottom-right");
+        GUI.Box(new Rect(20, 80, 465, 200), "");
+        scrollPosition = GUI.BeginScrollView(new Rect(20, 80, 465, 200), scrollPosition, new Rect(0, 0, 200, 200), false, true);
+
+        GUI.Button(new Rect(0, 0, 445, 20), "Top-left");
+        GUI.Button(new Rect(0, 21, 445, 20), "Top-left");
+        GUI.Button(new Rect(0, 43, 445, 20), "Top-left");
+        GUI.Button(new Rect(0, 65, 445, 20), "Top-left");
         GUI.EndScrollView();
 
 
+        GUI.Button(new Rect(360, 310, 120, 20), "Select image");
 /*
         //GUILayout.BeginVertical("hallo", GUILayout.MaxHeight(50));
         GUILayout.BeginScrollView(new Vector2(100, 100));
